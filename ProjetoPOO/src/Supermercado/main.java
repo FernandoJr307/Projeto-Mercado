2ce8ec91e64b865c0ca5da94f0450dc5a9266462
Fernando <fernando.formiga307@gmail.com>
2021-02-09 01:33:25 -0200

    Primeiro Commit

---
+++ ProjetoPOO/src/Supermercado/main.java
@@ -0,0 +1,20 @@
+package Supermercado;
+
+public class main {
+  
+    
+    public static void main(String[] args) {
+        Estoque estoqueLocal = new Estoque();
+        BD bd = new BD(estoqueLocal);
+        Arquivo arquivo = new Arquivo();
+        Threads[] threads = new Threads[1];
+        arquivo.getProdutos(estoqueLocal);
+        arquivo.getFuncionarios(bd);
+
+        for (int i = 0; i < 1; i++) {
+            threads[i] = new Threads(bd, estoqueLocal);
+            threads[i].run();
+        }
+    }
+    
+}
